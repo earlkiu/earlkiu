cc2060895331070a856ccd9fc61644ff17932bf9
earlkiu <itsme@earlkiu.com>
2026-05-23 14:26:25 +0800

    Update collections.js

--- collections.js
+++ collections.js
@@ -17,13 +17,14 @@
 // ─────────────────────────────────────────────
 
 const collections = [
-  {
-    title:    'Denise',
-    url:      '/collection/denise-2024-01.html',
+ {
+    title:    'Zyra',
+    url:      '/collection/zyra-2025-01.html',
     category: 'People',
-    year:     '2024',
-    cover:    'https://res.cloudinary.com/dljk05sju/image/upload/q_auto/f_auto/v1779382053/Denise_28-04-24_5451_fhm8jy.jpg'
+    year:     '2025',
+    cover:    'https://res.cloudinary.com/dljk05sju/image/upload/q_auto/f_auto/v1779119897/Zyra_03-05-25_10526_ivngdo.jpg'
   },
+  
   {
     title:    'Ming Xuan',
     url:      '/collection/ming-xuan-2025-01.html',
@@ -32,10 +33,10 @@ const collections = [
     cover:    'https://res.cloudinary.com/dljk05sju/image/upload/q_auto/f_auto/v1779116539/Ming-Xuan-14-08-25-0087_k8gptw.jpg'
   },
   {
-    title:    'Zyra',
-    url:      '/collection/zyra-2025-01.html',
+    title:    'Denise',
+    url:      '/collection/denise-2024-01.html',
     category: 'People',
-    year:     '2025',
-    cover:    'https://res.cloudinary.com/dljk05sju/image/upload/q_auto/f_auto/v1779119897/Zyra_03-05-25_10526_ivngdo.jpg'
+    year:     '2024',
+    cover:    'https://res.cloudinary.com/dljk05sju/image/upload/q_auto/f_auto/v1779382053/Denise_28-04-24_5451_fhm8jy.jpg'
   },
 ];
